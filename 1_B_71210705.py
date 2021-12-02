6b691c9ebc8a03537a64c5b5170b676aa5c1e050
michaeljovan <michael.jovan@ti.ukdw.ac.id>
2021-12-03 04:10:55 +0700

    ngerapiin

--- 1_B_71210705.py
+++ 1_B_71210705.py
@@ -1,13 +1,12 @@
 n = int(input("Masukkan banyak bilangan : "))
 i = 1
-
 print("Total = ", end="")
 
-for x in range(1, n + 1):
-    if x % 7 == 0:
+for x in range(1, n + 1) :
+    if x % 7 == 0 :
         i = i + 0
         print("+ 0", end=" ")
-    elif x % 3 == 0:
+    elif x % 3 == 0 :
         i = i + (x * -1)
         print("- " + str(x), end=" ")
     elif x == 1:
